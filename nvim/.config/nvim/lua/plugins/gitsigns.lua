a04a64d6f47ce8fe1bfa0b1de99d33a5c84e2170
Dartsac <idobson.gatech@gmail.com>
2024-12-13 18:28:00 -0500

    gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,5 @@
 return {
-  "lewis6991/gitsigns.nvim", -- Add the dependency for colorbuddy
+  "lewis6991/gitsigns.nvim",
   lazy = false,
   config = function()
     local status_ok, gitsigns = pcall(require, "gitsigns")
@@ -11,14 +11,19 @@ return {
 
     gitsigns.setup({
       signs = {
-        add = { text = icons.ui.BoldLineMiddle },
-        delete = { text = icons.ui.Triangle },
+        add          = { text = icons.ui.BoldLineMiddle },
+        delete       = { text = icons.ui.Triangle },
       },
+      signs_staged = {
+        add          = { text = icons.ui.BoldLineMiddle },
+        delete       = { text = icons.ui.Triangle },
+      },
+      signs_staged_enable = false,
+      attach_to_untracked = true,
       watch_gitdir = {
         interval = 1000,
         follow_files = true,
       },
-      attach_to_untracked = true,
       current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
       update_debounce = 200,
       max_file_length = 40000,
@@ -29,6 +34,8 @@ return {
         row = 0,
         col = 1,
       },
-    })
-  end
+      current_line_blame = false,
+      show_deleted = false,
+     })
+  end,
 }
