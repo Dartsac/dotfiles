53e3d17f5b12894c21081e93ade0f09c4f996f5f
Dartsac <idobson.gatech@gmail.com>
2024-12-13 22:32:04 -0500

    add mini.icons to my nvim-tree

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -7,7 +7,20 @@ if not status_ok then
 end
 
 local icons = require("config.icons")
--- require("user.miniicons")
+
+local miniicons = require("mini.icons")
+local icon, hl = miniicons.get("file", "file.lua")
+
+miniicons.setup({
+	style = "glyph", -- Use glyph icons
+	file = {
+		-- Explicitly set the icon for 'init.lua'
+		-- I like having init.lua with the same icon and hl as any other .lua file
+		["init.lua"] = { glyph = icon, hl = hl }, -- Match the Lua file icon
+	},
+})
+
+miniicons.mock_nvim_web_devicons()
 
 local function on_attach(bufnr)
   local api = require("nvim-tree.api")
