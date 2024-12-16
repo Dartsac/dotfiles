fa69806bb1a580677d3235946dbc8fd57e5e1cd6
Dartsac <idobson.gatech@gmail.com>
2024-12-16 15:07:40 -0500

    autopairs with autotag

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -2,33 +2,54 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
+		dependencies = {
+			{
+				"windwp/nvim-ts-autotag",
+				config = function()
+					require("nvim-ts-autotag").setup({
+						enable_close = true, -- Auto close tags
+						enable_rename = true, -- Auto rename pairs of tags
+						enable_close_on_slash = false, -- Auto close on trailing </
+						per_filetype = {
+							["html"] = {
+								enable_close = false,
+							},
+						},
+					})
+				end,
+			},
+		},
 		config = function()
-			-- Setup nvim-cmp.
-			local status_ok, npairs = pcall(require, "nvim-autopairs")
-			if not status_ok then
-				return
+			-- Helper function to check plugin availability
+			local safe_require = function(module)
+				local ok, lib = pcall(require, module)
+				return ok and lib or nil
 			end
 
-			local rule_ok, Rule = pcall(require, "nvim-autopairs.rule")
-			if not rule_ok then
+			-- Safely load required modules
+			local npairs = safe_require("nvim-autopairs")
+			if not npairs then
 				return
 			end
+			local Rule = safe_require("nvim-autopairs.rule")
+			local cond = safe_require("nvim-autopairs.conds")
+			local cmp = safe_require("cmp")
+			local cmp_autopairs = safe_require("nvim-autopairs.completion.cmp")
 
-			local cond = require("nvim-autopairs.conds")
-
+			-- Main nvim-autopairs setup
 			npairs.setup({
-				check_ts = true,
+				check_ts = true, -- Enable treesitter integration
 				ts_config = {
-					lua = { "string", "source" },
+					lua = { "string", "source" }, -- Exclude Lua strings
 					javascript = { "string", "template_string" },
-					java = false,
+					java = false, -- Disable treesitter for Java
 				},
 				disable_filetype = { "TelescopePrompt", "spectre_panel" },
 				fast_wrap = {
 					map = "<C-e>",
 					chars = { "{", "[", "(", '"', "'" },
 					pattern = string.gsub([[ [%'%"%)%>%]%)%}%,%;] ]], "%s+", ""),
-					offset = 0, -- Offset from pattern match
+					offset = 0,
 					end_key = "$",
 					keys = "qwertyuiopzxcvbnmasdfghjkl",
 					check_comma = true,
@@ -37,69 +58,68 @@ return {
 				},
 			})
 
-			npairs.add_rules({
-				Rule("%(.*%)%s*%=$", "> {}", { "typescript", "typescriptreact", "javascript" })
-					:use_regex(true)
-					:set_end_pair_length(1),
-				Rule("=", "")
-					:with_pair(cond.not_inside_quote())
-					:with_pair(function(opts)
-						local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
-						if last_char:match("[%w%=%s]") then
-							return true
-						end
-						return false
-					end)
-					:replace_endpair(function(opts)
-						local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
-						local next_char = opts.line:sub(opts.col, opts.col)
-						next_char = next_char == " " and "" or " "
-						if prev_2char:match("%w$") then
-							return "<bs> =" .. next_char
-						end
-						if prev_2char:match("%=$") then
-							return next_char
-						end
-						if prev_2char:match("=") then
-							return "<bs><bs>=" .. next_char
-						end
-						return ""
-					end)
-					:set_end_pair_length(0)
-					:with_move(cond.none())
-					:with_del(cond.none()),
-			})
-
-			local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
-			npairs.add_rules({
-				Rule(" ", " "):with_pair(function(opts)
-					local pair = opts.line:sub(opts.col - 1, opts.col)
-					return vim.tbl_contains({
-						brackets[1][1] .. brackets[1][2],
-						brackets[2][1] .. brackets[2][2],
-						brackets[3][1] .. brackets[3][2],
-					}, pair)
-				end),
-			})
-			for _, bracket in pairs(brackets) do
+			-- Add custom rules
+			if Rule and cond then
 				npairs.add_rules({
-					Rule(bracket[1] .. " ", " " .. bracket[2])
-						:with_pair(function()
-							return false
+					-- Rule for function body braces
+					Rule("%(.*%)%s*%=$", "> {}", { "typescript", "typescriptreact", "javascript" })
+						:use_regex(true)
+						:set_end_pair_length(1),
+
+					-- Rule for `=` pairs
+					Rule("=", "")
+						:with_pair(cond.not_inside_quote())
+						:with_pair(function(opts)
+							local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+							return last_char:match("[%w%=%s]") ~= nil
 						end)
-						:with_move(function(opts)
-							return opts.prev_char:match(".%" .. bracket[2]) ~= nil
+						:replace_endpair(function(opts)
+							local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+							local next_char = opts.line:sub(opts.col, opts.col) == " " and "" or " "
+							if prev_2char:match("%w$") then
+								return "<bs> =" .. next_char
+							end
+							if prev_2char:match("%=$") then
+								return next_char
+							end
+							if prev_2char:match("=") then
+								return "<bs><bs>=" .. next_char
+							end
+							return ""
 						end)
-						:use_key(bracket[2]),
+						:set_end_pair_length(0)
+						:with_move(cond.none())
+						:with_del(cond.none()),
 				})
+
+				-- Handle brackets with custom space rules
+				local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+				for _, bracket in ipairs(brackets) do
+					npairs.add_rules({
+						Rule(" ", " "):with_pair(function(opts)
+							local pair = opts.line:sub(opts.col - 1, opts.col)
+							return vim.tbl_contains({
+								brackets[1][1] .. brackets[1][2],
+								brackets[2][1] .. brackets[2][2],
+								brackets[3][1] .. brackets[3][2],
+							}, pair)
+						end),
+						Rule(bracket[1] .. " ", " " .. bracket[2])
+							:with_pair(function()
+								return false
+							end)
+							:with_move(function(opts)
+								return opts.prev_char:match(".%" .. bracket[2]) ~= nil
+							end)
+							:use_key(bracket[2]),
+					})
+				end
 			end
 
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp_status_ok, cmp = pcall(require, "cmp")
-			if not cmp_status_ok then
-				return
+			-- Integrate with nvim-cmp if available
+			if cmp and cmp_autopairs then
+				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 			end
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 		end,
 	},
 }
