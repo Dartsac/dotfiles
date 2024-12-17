a3e64515556aa44218411886e53d819bb65705ea
Dartsac <idobson.gatech@gmail.com>
2024-12-17 15:11:38 -0500

    add relative path to lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -58,7 +58,28 @@ return {
 							end,
 						},
 					},
-					lualine_c = {},
+					lualine_c = {
+						{
+							"filename",
+							file_status = true, -- Displays file status (readonly status, modified status)
+							newfile_status = false, -- Display new file status (new file means no write after created)
+							path = 1,
+							-- 0: Just the filename
+							-- 1: Relative path
+							-- 2: Absolute path
+							-- 3: Absolute path, with tilde as the home directory
+							-- 4: Filename and parent dir, with tilde as the home directory
+							shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+							-- for other components. (terrible name, any suggestions?)
+							symbols = {
+								modified = "[+]", -- Text to show when the file is modified.
+								readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+								unnamed = "[No Name]", -- Text to show for unnamed buffers.
+								newfile = "[New]", -- Text to show for newly created file before first write
+							},
+							color = { fg = "#F8F8F2" },
+						},
+					},
 					lualine_x = {
 						{
 							"diagnostics",
@@ -103,7 +124,7 @@ return {
 				inactive_sections = {
 					lualine_a = {},
 					lualine_b = {},
-					lualine_c = { "filename" },
+					lualine_c = {},
 					lualine_x = { "location" },
 					lualine_y = {},
 					lualine_z = {},
