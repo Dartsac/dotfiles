98732c1d2767030d1321cd423b964d40c6e7c6ee
Dartsac <idobson.gatech@gmail.com>
2024-12-16 22:20:15 -0500

    dadbod
    oh yeah baby

---
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"tpope/vim-dadbod",
+		lazy = true,
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = { "tpope/vim-dadbod" },
+		cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+	},
+	{
+		"kristijanhusak/vim-dadbod-completion",
+		dependencies = { "tpope/vim-dadbod" },
+		ft = { "sql", "mysql", "plsql" },
+	},
+}
