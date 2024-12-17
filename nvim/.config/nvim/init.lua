30a339e7f053bff326f309a024bd2ffb20621b1b
Dartsac <idobson.gatech@gmail.com>
2024-12-17 00:21:56 -0500

    clean up init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,33 +3,7 @@ require("config.autocommands")
 require("config.lazy")
 require("config.keymaps")
 
--- require("user.colorscheme")
---[[ require("user.colorizer") ]]
---[[ require("user.cmp") ]]
---[[ require("user.luasnip") ]]
---[[ require("user.lsp") ]]
---[[ require("user.telescope") ]]
---[[ require("user.gitsigns") ]]
---[[ require("user.treesitter") ]]
---[[ require("user.autopairs") ]]
---[[ require("user.autotag") ]]
---[[ require("user.template-string") ]]
---[[ require("user.comment") ]]
---[[ require("user.nvim-tree") ]]
---[[ require("user.bufferline") ]]
---[[ require("user.lualine") ]]
---[[ require("user.toggleterm") ]]
---[[ require("user.project") ]]
---[[ require("user.impatient") ]]
---[[ require("user.indentline") ]]
---[[ require("user.whichkey") ]]
---[[ require("user.autocommands") ]]
---[[ require("user.devicons") ]]
---[[ require("user.miniicons") ]]
---[[ require("user.illuminate") ]]
---[[ require("user.inc-rename") ]]
 vim.g.python3_host_prog = "~/.venvs/nvim/bin/python3"
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
-
 vim.g.dracula_colorterm = 0
