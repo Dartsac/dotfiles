b767c7fbdd957f28466f0d57ccacd676978cfc56
Dartsac <idobson.gatech@gmail.com>
2024-12-18 00:08:40 -0500

    inc-rename.lua

---
+++ nvim/.config/nvim/lua/plugins/inc-rename.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"smjonas/inc-rename.nvim",
+		config = function()
+			local status_ok, rename = pcall(require, "inc_rename")
+			if not status_ok then
+				return
+			end
+
+			rename.setup()
+
+			vim.keymap.set("n", "<leader>rn", function()
+				return ":IncRename " .. vim.fn.expand("<cword>")
+			end, { expr = true, desc = "Incremental rename", noremap = true })
+		end,
+	},
+}
