e783f12598b42a3146a552f48c4e3ac9529076cf
Dartsac <idobson.gatech@gmail.com>
2024-12-30 15:53:50 -0500

    adding fish format and diagnostics
    also migrate to blackd from black

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -1,18 +1,25 @@
 -- lua/lsp/null-ls.lua
 local M = {}
+local handlers = require("lsp.handlers")
 
 function M.setup()
 	local null_ls = require("null-ls")
 
 	local formatting = null_ls.builtins.formatting
-	-- local diagnostics = null_ls.builtins.diagnostics
+	local diagnostics = null_ls.builtins.diagnostics
 
 	null_ls.setup({
 		sources = {
 			formatting.prettierd.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-			formatting.black.with({ extra_args = { "--fast" } }),
+			formatting.blackd.with({ extra_args = { "--fast" } }),
 			formatting.stylua,
+			formatting.fish_indent,
+
+			diagnostics.fish,
 		},
+		on_attach = function(client, bufnr)
+			handlers.on_attach(client, bufnr)
+		end,
 	})
 end
 
