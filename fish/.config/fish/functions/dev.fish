e7558cfe3bc82017b74f2e89313e76f0ac7edae8
Dartsac <idobson.gatech@gmail.com>
2025-02-27 12:45:11 -0500

    update dev session script

--- fish/.config/fish/functions/dev.fish
+++ fish/.config/fish/functions/dev.fish
@@ -1,14 +1,16 @@
+# ~/.config/fish/functions/dev.fish
 function dev
+    # Check if in tmux already
     if test -n "$TMUX"
-        # Inside a tmux session: split and configure panes
+        # Inside a tmux session: set up the panes
         tmux split-window -v -p 20
         set top_pane (tmux list-panes -F '#{pane_id} #{pane_top}' | sort -nk2 | head -n1 | awk '{print $1}')
         tmux send-keys -t $top_pane 'nvim .' C-m
     else
-        # Run your exact tmux command
-        set selected_name (basename (pwd))
-        tmux new-session -s $selected_name -c (pwd) -n $selected_name -A\; \
-            send-keys 'v' C-m \; \
+        # Create a new session with a good name
+        set session_name (basename (pwd))
+        tmux new-session -s $session_name -c (pwd) -n $session_name -A\; \
+            send-keys 'nvim .' C-m \; \
             split-window -v -p 20 \;
     end
 end
