c40a739beb47d9d82ec6889ebab175691a23f698
Dartsac <idobson.gatech@gmail.com>
2025-05-09 16:51:40 -0400

    add missing fields

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,18 +1,21 @@
 return {
-	{
-		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-		event = "BufReadPost",
-		config = function()
-			-- Ensure ts_context_commentstring is set up first
-			require("ts_context_commentstring").setup({
-				enable_autocmd = false, -- Avoid unnecessary autocmd triggers
-			})
+	"numToStr/Comment.nvim",
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	event = "BufReadPost",
+	config = function()
+		require("ts_context_commentstring").setup({ enable_autocmd = false })
 
-			-- Simplified Comment.nvim setup
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
-		end,
-	},
+		---@diagnostic disable: missing-fields
+		require("Comment").setup({
+			padding = true,
+			sticky = true,
+			mappings = { basic = true, extra = false },
+			toggler = { line = "gcc", block = "gbc" },
+			opleader = { line = "gc", block = "gb" },
+			extra = { above = "gcO", below = "gco", eol = "gcA" },
+
+			-- keep ts‑context‑commentstring working
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
 }
