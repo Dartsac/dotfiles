25f08a6f46346952505cecbe33d3478e525767e0
Dartsac <idobson.gatech@gmail.com>
2025-05-12 16:36:20 -0400

    refactor(whichkey): standardize LSP and Search group mappings

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -94,28 +94,144 @@ return {
 					desc = "Workspace Symbols",
 					mode = "n",
 				},
-				{ "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action", mode = "n" },
-				{ "<leader>ld", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics", mode = "n" },
-				{ "<leader>lf", "<cmd>lua vim.lsp.buf.format{async=true}<cr>", desc = "Format", mode = "n" },
-				{ "<leader>li", "<cmd>LspInfo<cr>", desc = "Info", mode = "n" },
-				{ "<leader>lj", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", desc = "Next Diagnostic", mode = "n" },
-				{ "<leader>lk", "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", desc = "Prev Diagnostic", mode = "n" },
-				{ "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", desc = "CodeLens Action", mode = "n" },
-				{ "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<cr>", desc = "Quickfix", mode = "n" },
-				{ "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename", mode = "n" },
-				{ "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols", mode = "n" },
-				{ "<leader>lw", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics", mode = "n" },
+				{
+					"<leader>la",
+					"<cmd>lua vim.lsp.buf.code_action()<cr>",
+					desc = "Code Action",
+					mode = "n",
+				},
+				{
+					"<leader>ld",
+					"<cmd>Telescope diagnostics bufnr=0<cr>",
+					desc = "Document Diagnostics",
+					mode = "n",
+				},
+				{
+					"<leader>lf",
+					function()
+						local clients = vim.lsp.get_clients({ bufnr = 0 })
+						local has_null = vim.tbl_contains(
+							vim.tbl_map(function(c)
+								return c.name
+							end, clients),
+							"null-ls"
+						)
+
+						vim.lsp.buf.format({
+							async = true,
+							filter = function(client)
+								if has_null then
+									-- if null-ls is present, restrict to null-ls only
+									return client.name == "null-ls"
+								else
+									-- otherwise allow any formatter
+									return true
+								end
+							end,
+						})
+					end,
+					desc = "Format (null-ls preferred)",
+					mode = "n",
+				},
+				{
+					"<leader>li",
+					"<cmd>LspInfo<cr>",
+					desc = "Info",
+					mode = "n",
+				},
+				{
+					"<leader>lj",
+					"<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
+					desc = "Next Diagnostic",
+					mode = "n",
+				},
+				{
+					"<leader>lk",
+					"<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>",
+					desc = "Prev Diagnostic",
+					mode = "n",
+				},
+				{
+					"<leader>ll",
+					"<cmd>lua vim.lsp.codelens.run()<cr>",
+					desc = "CodeLens Action",
+					mode = "n",
+				},
+				{
+					"<leader>lq",
+					"<cmd>lua vim.diagnostic.setloclist()<cr>",
+					desc = "Quickfix",
+					mode = "n",
+				},
+				{
+					"<leader>lr",
+					"<cmd>lua vim.lsp.buf.rename()<cr>",
+					desc = "Rename",
+					mode = "n",
+				},
+				{
+					"<leader>ls",
+					"<cmd>Telescope lsp_document_symbols<cr>",
+					desc = "Document Symbols",
+					mode = "n",
+				},
+				{
+					"<leader>lw",
+					"<cmd>Telescope diagnostics<cr>",
+					desc = "Workspace Diagnostics",
+					mode = "n",
+				},
 
 				-- Search Group
 				{ "<leader>s", group = "Search" },
-				{ "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands", mode = "n" },
-				{ "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages", mode = "n" },
-				{ "<leader>sR", "<cmd>Telescope registers<cr>", desc = "Registers", mode = "n" },
-				{ "<leader>sb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch", mode = "n" },
-				{ "<leader>sc", "<cmd>Telescope colorscheme<cr>", desc = "Colorscheme", mode = "n" },
-				{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Find Help", mode = "n" },
-				{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps", mode = "n" },
-				{ "<leader>sr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File", mode = "n" },
+				{
+					"<leader>sC",
+					"<cmd>Telescope commands<cr>",
+					desc = "Commands",
+					mode = "n",
+				},
+				{
+					"<leader>sM",
+					"<cmd>Telescope man_pages<cr>",
+					desc = "Man Pages",
+					mode = "n",
+				},
+				{
+					"<leader>sR",
+					"<cmd>Telescope registers<cr>",
+					desc = "Registers",
+					mode = "n",
+				},
+				{
+					"<leader>sb",
+					"<cmd>Telescope git_branches<cr>",
+					desc = "Checkout branch",
+					mode = "n",
+				},
+				{
+					"<leader>sc",
+					"<cmd>Telescope colorscheme<cr>",
+					desc = "Colorscheme",
+					mode = "n",
+				},
+				{
+					"<leader>sh",
+					"<cmd>Telescope help_tags<cr>",
+					desc = "Find Help",
+					mode = "n",
+				},
+				{
+					"<leader>sk",
+					"<cmd>Telescope keymaps<cr>",
+					desc = "Keymaps",
+					mode = "n",
+				},
+				{
+					"<leader>sr",
+					"<cmd>Telescope oldfiles<cr>",
+					desc = "Open Recent File",
+					mode = "n",
+				},
 
 				-- Git signs
 				{ "<leader>g", group = "Git" },
