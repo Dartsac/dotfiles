8026a1ed425071e1308167478def209f58fa5c00
Dartsac <idobson.gatech@gmail.com>
2025-11-22 17:50:54 -0500

    [NVIM] fix attach to file I vim into instead of the harpooned file

--- nvim/.config/nvim/lua/config/attach.lua
+++ nvim/.config/nvim/lua/config/attach.lua
@@ -1,10 +1,19 @@
 -- lua/config/attach.lua
+
+-- Only auto-attach to harpoon if no file was explicitly opened
+local has_file_arg = vim.fn.argc() > 0
+
+if has_file_arg then
+	-- User opened a specific file, don't auto-attach to harpoon
+	return
+end
+
 local ok, harpoon = pcall(require, "harpoon")
 if not ok or vim.tbl_isempty(harpoon:list().items) then
 	return
 end
 
--- Don’t show the splash
+-- Don't show the splash
 vim.opt.shortmess:append("I")
 -- Jump *after* all startup-autocmds run
 vim.schedule(function()
